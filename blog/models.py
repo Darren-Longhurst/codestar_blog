5ee2e7fd27d1d0d609c6b182c06df975bc251392
Darren Longhurst <dmlonghurst@icloud.com>
2025-12-09 01:19:39 +0000

    Admin panel powerup

--- blog/models.py
+++ blog/models.py
@@ -1,36 +1,37 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils import timezone
- # Import models to connect
+from django.contrib.auth.models import User # Import models to connect
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True, default="Untitled Post")
-    slug = models.SlugField(max_length=200, unique=True, default="Untitled Post")
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts", default=1)
-    content = models.TextField(default="No content provided")
-    created_on = models.DateTimeField(default=timezone.now)
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True, default="")
-    updated_on = models.DateTimeField(default=timezone.now)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_on"]
-
+    
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
-    name = models.CharField(max_length=80, default="Anonymous")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
-    created_on = models.DateTimeField(default=timezone.now)
+    created_on = models.DateTimeField(auto_now_add=True)
+    new_field = models.SlugField(default='default-value')
 
     class Meta:
         ordering = ["created_on"]
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,6 @@ from django.views import generic
 from .models import Post
 
 # Create your views here.
-
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
